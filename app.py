c712387325c1a6e9c54e137ee107a41c5319033a
minhntd256 <duyminhnt256@gmail.com>
2022-10-28 15:06:11 +0700

    update

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask
+
+from flask import Flask, request
+import json
+import base64
+from model import model_fit
+
 app = Flask(__name__)
-import os
 
 @app.route('/')
 def hello_world():
-    
-
     return 'Hello Sammy!'
 
-# @app.route("/model",methods=['POST'])
-# def predict():
-#     return {"answer":"Hello"}
+@app.route("/model",methods=['POST'])
+def predict():
+        data = request.json
+        imgdata = base64.b64decode(data["image"].split(',')[1])
+        filename = 'image.jpg'
+        with open(filename, 'wb') as f:
+                f.write(imgdata)
+        result = model_fit(data['question'], 'image.jpg')
+        answer = "Sorry, I can't answer your question. Please ask another question."
+        if result == 1:
+                answer = "Ok, I can answer your question."
+        json_object = json.dumps({"message": answer}) 
+        return json_object
